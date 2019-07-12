85db0fa1caba8eaeae178abd5258dfad32d4bef9
Alexander <terraforwebdev@gmail.com>
2019-07-12 23:54:00 +0300

    repair dropdown element

--- Common.blocks/InputBlock/__Dropdown/InputBlock__Dropdown.js
+++ Common.blocks/InputBlock/__Dropdown/InputBlock__Dropdown.js
@@ -4,8 +4,8 @@ $("#filesB").multiselect({
   noneSelectedText: ' ',
   classes: 'InputBlock__InputField InputBlock_Width_265px InputBlock_Appearan_none InputBlock_PdTop_11px InputBlock_LetterSpacing_0px InputBlock_MrBot_19px InputBlock_Color_Dark75',
 	selectedList: 2,
-  //listbox: 10,
 	selectedText: function(numChecked, numTotal, checkedItems){
+    this.$button[0].children[0].children[0].outerHTML = '<i class="material-icons">expand_more</i>'; 
     value = this.$inputs.filter(':checked').map(function() { 
 
       var valInputNumber = $(this).siblings('.quantity-block').children('input').val();
@@ -14,7 +14,7 @@ $("#filesB").multiselect({
       } else {
         valInputNumber += " ";
       }
-      return $(this).next().text().replace(/\n$/, '').replace(/^/, valInputNumber) 
+      return $(this).next().text().replace(/\n$/, '').replace(/^/, valInputNumber); 
 
     }).get().join( this.options.selectedListSeparator);
 
@@ -22,8 +22,6 @@ $("#filesB").multiselect({
       var str = value.match( /(^[^,]+,[^,]+)/i );
       value = str[0] + '...';
     }
-    this.$button[0].children[0].children[0].outerHTML = '<i class="material-icons">expand_more</i>'; 
-    console.log(this);
 
     return value;
 	},
@@ -36,21 +34,32 @@ $('.ui-multiselect-checkboxes label')
           '<button class="quantity-arrow-plus"> + </button>' +
         '</div>');
 
-//console.log($('input[name=multiselect_filesB]'));
 $('.ui-multiselect-checkboxes input[type=checkbox]').each(function(index) {
-  console.log($(this));
   $(this).siblings('.quantity-block').children('input').attr('name', $(this).val());
 });
 
 $(".quantity-arrow-plus").click(function(eventObject){
   var targetPlus = $(this).prev();
   targetPlus.val( +targetPlus.val() + 1 );
- // $('.ui-multiselect').children('span').last().prepend(targetPlus.val() + ' ');
+
+  var InputNameAttr = targetPlus.attr('name');
+  var optinsContent = $('#filesB option[value='+InputNameAttr+']').text();
+  var dropDownText = $('.ui-multiselect span:last').text();
+  var regular = new RegExp('\\d+ ' + optinsContent, 'i');
+  var modifiedText = dropDownText.replace( regular, targetPlus.val() + ' ' + optinsContent );
+  $('.ui-multiselect span:last').text(modifiedText);
 });
 
 $(".quantity-arrow-minus").click(function(eventObject){
   var targetMinus = $(this).next();
   if (targetMinus.val() > 0) {
     targetMinus.val( +targetMinus.val() - 1 );
-    }
+
+    var InputNameAttr = targetMinus.attr('name');
+    var optinsContent = $('#filesB option[value='+InputNameAttr+']').text();
+    var dropDownText = $('.ui-multiselect span:last').text();
+    var regular = new RegExp('\\d+ ' + optinsContent, 'i');
+    var modifiedText = dropDownText.replace( regular, targetMinus.val() + ' ' + optinsContent );
+    $('.ui-multiselect span:last').text(modifiedText);
+  }
 });
