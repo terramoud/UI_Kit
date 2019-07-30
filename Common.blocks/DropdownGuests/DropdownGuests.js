2c0a7473f0d5f4295fea7d2f05e71d339adbfba2
Alexander <terraforwebdev@gmail.com>
2019-07-30 19:46:25 +0300

    added clear button

--- Common.blocks/DropdownGuests/DropdownGuests.js
+++ Common.blocks/DropdownGuests/DropdownGuests.js
@@ -27,81 +27,69 @@
 *   beds == "2"
 */
 
+
 /** 
 * Namespace
 */
 var dropdownGuests = {
   widgetClass: '.DropdownGuests',
   id:  $('select.DropdownGuests').attr('id'),
+  noneSelectedText: 'Сколько гостей',
+  applyButton: 'применить',
+  clearButton: 'очистить',
 };
+
 
 /** 
 * Includs widget multiselect 
 */
-var noneSelectedText = 'Сколько гостей';
-
 $(dropdownGuests.widgetClass).multiselect({
   create: function(argument) {
+
     /** 
-    * 
+    * it is creating the expand more arrow instead the default arrow
     */
     $(this).next().find('.ui-multiselect-open').prepend('<i class="material-icons">expand_more</i>').children('.ui-icon').remove();
+
   },
-  click: function(event, ui){ // чтобы при нажатии только на плюс и минус могли выбартьсЯ оттрибуты
+
+  click: function(event, ui){ // it is deleting the click handler for the widget, now we can will be creating the custom handler
     return false; 
   },
+
   buttonWidth: 'auto',
-  noneSelectedText: noneSelectedText,
+  noneSelectedText: dropdownGuests.noneSelectedText,
   open: function(){
     $('.ui-multiselect-checkboxes').css('height', 'auto');
   },
-  //classes: 'InputBlock_Color_Dark75 InputBlock_MrBot_19px InputBlock_LetterSpacing_0px InputBlock_PdTop_11px InputBlock_Appearan_none InputBlock_Width_265px Dropdown',
-  //selectedList: 1,
-  selectedText: function(numChecked, numTotal, checkedItems){
-
-    var sumValInput = 0;
-
-    this.$inputs.map(function() { 
-      /** 
-      * This adds the number of selected items before name of the this item in widget field
-      */
-      var valInputNumber = $(this).siblings('.QuantityBlock').children('input').val();
-      sumValInput += +valInputNumber;
-/*      if (!valInputNumber) {
-        valInputNumber = "0";
-      } else {
-        valInputNumber += " ";
-      }*/      
-    }).get();
-
-    value = sumValInput + ' Гостей';
-
-    return value + "adsfasdf'akdjsf";
-  },
-
-}).multiselect("getButton");
+});
 
 
 /** 
-* 
+*  it is swapping multiselect header
 */
 var header = $('.ui-multiselect-checkboxes label[for *= "' + dropdownGuests.id + '"]').parents('.ui-multiselect-menu').children('.ui-multiselect-header').detach();
 $('.ui-multiselect-checkboxes label[for *= "' + dropdownGuests.id + '"]').parents('.ui-multiselect-menu').append(header);
 
+
 /** 
-* 
+* it is removing unnecessary items from the header
 */
 $('.ui-multiselect-checkboxes label[for *= "' + dropdownGuests.id + '"]').parents('.ui-multiselect-menu').find('li.ui-multiselect-close').remove();
 
+
 /** 
-* 
+* it swapps default buttons   
 */
 var replacedButton = $('.ui-multiselect-checkboxes label[for *= "' + dropdownGuests.id + '"]').parents('.ui-multiselect-menu').find('.ui-multiselect-header li:first').detach();
 $('.ui-multiselect-checkboxes label[for *= "' + dropdownGuests.id + '"]').parents('.ui-multiselect-menu').find('.ui-multiselect-header .ui-helper-reset').append(replacedButton);
 
-/*создаем кнопку применить Удаление обработчиков ччобытий */
+
+/** 
+*  it is creating the apply button instead the default button and this delete the handlers of events
+*/
 $('.ui-multiselect-checkboxes label[for *= "' + dropdownGuests.id + '"]').parents('.ui-multiselect-menu').find('.ui-multiselect-header li:last').html(function(indx, oldHtml) {
-  return '<button class="ui-multiselect-apply" type="submit" form="form1" value="Submit">Применить</button>'
+  return '<button class="ui-multiselect-apply" type="submit" form="form1" value="Submit">' + dropdownGuests.applyButton + '</button>'
 });
 
 
@@ -112,46 +100,37 @@ $(dropdownGuests.widgetClass).each(function(index, el) {
   $(this).next().addClass( $(this).attr('class') );
 });
 
+
 /** 
-* Creating custom buttons instead of default buttons in <input type="number"> 
+* Creating custom buttons instead of default buttons in <input type="text"> 
 */
 $('.ui-multiselect-checkboxes label[for *= "' + dropdownGuests.id + '"]').each(function(index, el) {
+  $(this).append(
+                    '<div class="QuantityBlock">' +
+                      '<button class="QuantityBlock__IconMinus"> - </button>' +
+                      '<input class="QuantityBlock__Num" type="text" placeholder="0" />' +
+                      '<button class="QuantityBlock__IconPlus"> + </button>' +
+                    '</div>'
+                  );
+  });
 
-  //$(this).children('input').attr('type', 'hidden');
-
-  $(this).append('<div class="QuantityBlock">' +
-                '<button class="QuantityBlock__IconMinus"> - </button>' +
-                '<input class="QuantityBlock__Num" type="text" placeholder="0" />' +
-                '<button class="QuantityBlock__IconPlus"> + </button>' +
-               '</div>');
-
-   /* var $elem = $(this).parent('li').data(  ); // оригинал элемента с закрепленным за ним массивом
-    var $clone = $(this).parent('li').clone() // создадим копию элемента
-      .data($elem);
-    var thisParentUl = $(this).parents('ul');
-    $(this).parent('li').remove();
-    thisParentUl.append($clone);
+  /** 
+  * This passes to the attribute 'name' the value that was obtained from the widget menu <input> tags
   */
+  $('.ui-multiselect-checkboxes input[type=checkbox]').each(function(index) {
+    $(this).siblings('.QuantityBlock').children('input').attr('name', $(this).val());
+    $(this).siblings('.QuantityBlock').children('input').val('0'); // It is fixed the bag that happenes after was being clicked on this input field
+  });
 
-
-
+  /** 
+  * This adds additional handlers for the click event. This changes the text of the widget to text when nothing is selected and resets the value of the input field.
+  */
+  $("label[for *= '" + dropdownGuests.id + "']").parents('.ui-multiselect-menu').find('.ui-multiselect-none').click(function(event) {
+    $('#' + dropdownGuests.id + '_ms span:last').text(dropdownGuests.noneSelectedText);
+    $('.QuantityBlock__Num').val(0);
+    $(this).css('visibility', 'hidden');
 });
 
-/** 
-* This passes to the attribute 'name' the value that was obtained from the widget menu <input> tags
-*/
-$('.ui-multiselect-checkboxes input[type=checkbox]').each(function(index) {
-  $(this).siblings('.QuantityBlock').children('input').attr('name', $(this).val());
-  $(this).siblings('.QuantityBlock').children('input').val('0'); // чтоьы не вылезало поле инпукта сбоку при клике
-});
-
-/** 
-* 
-*/
-$("label[for *= '" + dropdownGuests.id + "']").parents('.ui-multiselect-menu').find('.ui-multiselect-none').click(function(event) {
-  $('#' + dropdownGuests.id + '_ms span:last').text(noneSelectedText);
-  $('.QuantityBlock__Num').val(0);
-});
 
 /** 
 * This events are adding count of selected items in multiselect widget
@@ -161,7 +140,12 @@ $("label[for *= '" + dropdownGuests.id + "']").find(".QuantityBlock__IconPlus").
   targetPlus.val( +targetPlus.val() + 1 ); //This adds the number of selected elements in the “value” attribute of the “input” tag.
 
   /** 
-  * This is changes text in 'widget field' the by adding number of selected elements using a regular expression
+  * It's restoring the clear button
+  */
+  $(this).parents('.ui-multiselect-menu').find('.ui-multiselect-none').css('visibility', 'inherit');
+
+  /** 
+  * It's get the a unique identifier for every the dropdownGuests widget
   */
   var objectInput = $(this).parent('.QuantityBlock').siblings('input');
   var widgetId = objectInput.attr('id');
@@ -175,23 +159,16 @@ $("label[for *= '" + dropdownGuests.id + "']").find(".QuantityBlock__IconPlus").
   /** 
   * This is adding "checked" attribute for is passes "name" attribute for backend
   */
-
   objectInput.prop('checked', 'checked');
+
+  /** 
+  * This is changes text in 'widget field'
+  */
   $('#' + widgetId[1] + '_ms span:last').text(commonSumInputs + ' Гостей');
 
-/*  if (commonSumInputs <= 0) {
-    $('.ui-multiselect-checkboxes input[type=checkbox]').prop('checked', false);
-    console.log('remove all+ checked');
-  } else {
-    objectInput.prop('checked', 'checked');
-    console.log("+" + objectInput[0].checked );0
-    $('#' + widgetId[1] + '_ms span:last').text(commonSumInputs + ' Гостей');
-  }*/
-
   targetPlus = widgetId = commonSumInputs = modifiedText = false;
-
-
 });
+
 
 $("label[for *= '" + dropdownGuests.id + "']").find(".QuantityBlock__IconMinus").click(function(eventObject){
   var targetMinus = $(this).next();
@@ -199,7 +176,7 @@ $("label[for *= '" + dropdownGuests.id + "']").find(".QuantityBlock__IconMinus")
     targetMinus.val( +targetMinus.val() - 1 );
 
     /** 
-    * This is changes text in 'widget field' the by adding number of selected elements using a regular expression
+    * It's get the a unique identifier for every the dropdownGuests widget
     */
     var objectInput = $(this).parent('.QuantityBlock').siblings('input');
     var widgetId = objectInput.attr('id');
@@ -215,30 +192,33 @@ $("label[for *= '" + dropdownGuests.id + "']").find(".QuantityBlock__IconMinus")
     */
     if (targetMinus.val() <= 0) {
       objectInput.prop('checked', false);
-      console.log('remove 1 checked');
-      console.log( objectInput[0].checked );
     }
-    /** 
-    * 
-    */
+
+
     if (commonSumInputs <= 0) {
+
+      /** 
+      * This changes the text of the widget to text when nothing is selected and resets the value of the input field. 
+      */
       $('.ui-multiselect-checkboxes input[type=checkbox]').prop('checked', false);
-      console.log('remove all checked');
-      $('#' + widgetId[1] + '_ms span:last').text(noneSelectedText);
+      $('#' + widgetId[1] + '_ms span:last').text(dropdownGuests.noneSelectedText);
+
+      /** 
+      * It's restoring the clear button
+      */
+      $(this).parents('.ui-multiselect-menu').find('.ui-multiselect-none').css('visibility', 'hidden');
+
     } else {
-      console.log( objectInput[0].checked );
+
+      /** 
+      * This is changes text in 'widget field'
+      */
       $('#' + widgetId[1] + '_ms span:last').text(commonSumInputs + ' Гостей');
+
     }
 
     targetPlus = widgetId = commonSumInputs = modifiedText = false;
   }
-
-
 });
 
 // очистить применить нужно сделать
-/*
-удалить логи
-написать коменты к коду
-arrow down
-*/
