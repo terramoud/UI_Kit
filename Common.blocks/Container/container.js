eee653339dfbf1d570ff2ad67f22bc4a4169bdfe
Alexandr Koreshev <terramoud@gmail.com>
2020-08-13 01:04:02 +0300

    changed bemf.js utility for component architecture

--- Common.blocks/Container/container.js
+++ Common.blocks/Container/container.js
@@ -1,3 +1,5 @@
 require('./__layout/container__layout.js');
 require('./__layout/_theme/container__layout_theme_colors-fonts.js');
 require('./__layout-logo/container__layout-logo.js');
+
+require('./__layout-examples/container__layout-examples.js');
--- bemf — копия.js
+++ bemf — копия.js
@@ -16,7 +16,6 @@ blockName = ( !blockName )? '' : blockName[0] + '/';
 elementName = ( !elementName )? '' : elementName[0] + '/';
 modName = ( !modName )? '' : modName[1] + '/';
 fullPath = blockName + elementName + modName + createFileName;
-elemPath = elementName + modName + createFileName;
 console.log('../' + overrideLevel + blockName + elementName + modName + createFileName);
 
 isModifier = createFileName.match(/^[^_]+(_[^_ ]+)/i); // deprecated
@@ -48,49 +47,39 @@ try {
   console.error(err)
 }
 
-/* JS */
-if (elementName === '' && modName === '') {
-  var dataJs = "\nrequire('../" + overrideLevel + fullPath + ".js');";
-  //require('../Common.blocks/Content/__WrapperForFormElements/Content__WrapperForFormElements.js');
-  fs.appendFile('pages/' + entryPoint + '.js', dataJs, 'utf8',
-    // callback function
-    function(err) {
-      if (err) throw err;
-      // if no error
-      console.log("js appended.")
-    });
-}
+var dataJs = "\nrequire('../" + overrideLevel + fullPath + ".js');";
+//require('../Common.blocks/Content/__WrapperForFormElements/Content__WrapperForFormElements.js');
+fs.appendFile('pages/' + entryPoint + '.js', dataJs, 'utf8',
+  // callback function
+  function(err) {
+    if (err) throw err;
+    // if no error
+    console.log("js appended.")
+  });
 
-fs.writeFile(overrideLevel + fullPath + '.js', '', function (err) {
+fs.writeFile( overrideLevel + fullPath + '.js', '', function (err) {
   if (err) throw err;
   console.log(`${createFileName}.js is created successfully.`);
 });
 
-if (elementName !== '' || modName !== '') {
-  var dataJsElem = "\nrequire('./" + elemPath + ".js');";
-  fs.appendFile(overrideLevel + blockName + blockName.replace('/','') + '.js', dataJsElem, 'utf8',
-    // callback function
-    function(err) {
-      if (err) throw err;
-      // if no error
-      console.log("js appended.")
-    });
-}
-/* end JS */
+var dataStyle = "\n@import '../" + overrideLevel + fullPath + "';";
+fs.appendFile('pages/' + entryPoint + '.scss', dataStyle, 'utf8',
+  // callback function
+  function(err) {
+    if (err) throw err;
+    // if no error
+    console.log("scss appended.")
+  });
 
-/* SCSS */
-if (elementName === '' && modName === '') {
-  var dataStyle = "\n@import '../" + overrideLevel + fullPath + "';";
-  fs.appendFile('pages/' + entryPoint + '.scss', dataStyle, 'utf8',
-    // callback function
-    function(err) {
-      if (err) throw err;
-      // if no error
-      console.log("scss appended.")
-    });
-}
-
-
+// var scssData =  '@media only screen and (max-width: 320px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
+//   '@media only screen and (min-width: 321px) and (max-width: 575px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
+//   '@media only screen and (min-width: 576px) and (max-width: 767px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
+//   '@media only screen and (min-width: 768px) and (max-width: 991px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
+//   '@media only screen and (min-width: 992px) and (max-width: 1199px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
+//   '@media only screen and (min-width: 1200px) and (max-width: 1439px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
+//   '@media only screen and (min-width: 1440px) and (max-width: 1440px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
+//   '@media only screen and (min-width: 1441px) and (max-width: 1680px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
+//   '@media only screen and (min-width: 1681px) and (max-width: 1920px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n';
 
 var scssData =  '.' + createFileName + ' {\n\n}\n\n' +
                 '@media only screen and (min-width: 321px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
@@ -99,26 +88,12 @@ var scssData =  '.' + createFileName + ' {\n\n}\n\n' +
                 '@media only screen and (min-width: 992px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
                 '@media only screen and (min-width: 1200px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
                 '@media only screen and (min-width: 1440px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
-                '@media only screen and (min-width: 1681px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
                 '@media only screen and (min-width: 1681px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n';
 
 fs.writeFile( overrideLevel + fullPath + '.scss', scssData, function (err) {
   if (err) throw err;
   console.log(`${createFileName}.scss is created successfully.`);
 });
-
-if (elementName !== '' || modName !== '') {
-  var dataStyleElem = "\n@import './" + elemPath + "';";
-  fs.appendFile(overrideLevel + blockName + blockName.replace('/','') + '.scss', dataStyleElem, 'utf8',
-    // callback function
-    function(err) {
-      if (err) throw err;
-      // if no error
-      console.log("scss appended to bem block.")
-    });
-}
-/* end SCSS */
-
 
 // if ( !isModifier && modName === '' ) { - deprecated
 if ( modName === '' ) {
@@ -134,8 +109,17 @@ if ( modName === '' ) {
     console.log(`${createFileName}.pug is created successfully.`);
   });
 
+  prependFile('pages/' + entryPoint + '.pug', 'include ../' + overrideLevel + fullPath + '.pug\n', function (err) {
+    if (err) {
+      // Error
+    }
+
+    // Success
+    console.log('pug was prepended to file!');
+  });
+
   var data = '\n+' + createFileName;
-  // append data to file
+// append data to file
   fs.appendFile('pages/' + entryPoint + '.pug', data, 'utf8',
     // callback function
     function(err) {
@@ -144,35 +128,4 @@ if ( modName === '' ) {
       console.log("pug appended")
     });
 
-  if (elementName === '') {
-    prependFile('pages/' + entryPoint + '.pug', 'include ../' + overrideLevel + fullPath + '.pug\n', function (err) {
-      if (err) {
-        // Error
-      }
-      // Success
-      console.log('pug was prepended to file!');
-    });
-  }
-
-  if (elementName !== '') {
-    prependFile(overrideLevel + blockName + blockName.replace('/','') + '.pug', 'include ./' + elemPath + '.pug\n', function (err) {
-      if (err) {
-        // Error
-      }
-      // Success
-      console.log('pug was prepended to file!');
-    });
-  }
-
 }
-
-
-// var scssData =  '@media only screen and (max-width: 320px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
-//   '@media only screen and (min-width: 321px) and (max-width: 575px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
-//   '@media only screen and (min-width: 576px) and (max-width: 767px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
-//   '@media only screen and (min-width: 768px) and (max-width: 991px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
-//   '@media only screen and (min-width: 992px) and (max-width: 1199px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
-//   '@media only screen and (min-width: 1200px) and (max-width: 1439px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
-//   '@media only screen and (min-width: 1440px) and (max-width: 1440px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
-//   '@media only screen and (min-width: 1441px) and (max-width: 1680px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n' +
-//   '@media only screen and (min-width: 1681px) and (max-width: 1920px) {\n  .' + createFileName + ' {\n\n  }\n}\n\n';
