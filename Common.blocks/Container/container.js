b0e06986163de39fde9d0a5faa1dc9b784a59f98
Alexandr Koreshev <terramoud@gmail.com>
2020-08-16 18:47:11 +0300

    refactoring was carried out to fix the added early components and elements

--- Common.blocks/Container/container.js
+++
@@ -1,7 +0,0 @@
-require('./__layout/container__layout.js');
-require('./__layout/_theme/container__layout_theme_colors-fonts.js');
-require('./__layout-logo/container__layout-logo.js');
-require('./__layout-examples/container__layout-examples.js');
-require('./__layout/_theme/container__layout_theme_colors-fonts.js');
-require('./__layout-colors/container__layout-colors.js');
-require('./__layout-fonts/container__layout-fonts.js');
