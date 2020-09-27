5897e5b64a24c43652dcd2153f1e9b9c5737a421
Alexander <terramoud@gmail.com>
2020-09-27 19:29:39 +0300

    commit prepend push

--- Common.blocks/radio-buttons/radio-buttons.js
+++ Common.blocks/radio-buttons/radio-buttons.js
@@ -1,5 +1,4 @@
 require('./__wrapper/radio-buttons__wrapper.js');
 require('./__label/radio-buttons__label.js');
 require('./__radio-button/radio-buttons__radio-button.js');
-require('./__radio-button/_active/radio-buttons__radio-button_active.js');
 require('./__radio-button/_state/radio-buttons__radio-button_state_active.js');
